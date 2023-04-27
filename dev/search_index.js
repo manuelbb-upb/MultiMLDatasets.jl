9149ab703d8a870edc13697c25f408bd113affe6
Documenter.jl <documenter@juliadocs.github.io>
2023-04-27 09:29:41 +0000

    build based on 78795fd

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MultiMLDatasets","category":"page"},{"location":"#MultiMLDatasets","page":"Home","title":"MultiMLDatasets","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MultiMLDatasets.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MultiMLDatasets]","category":"page"}]
+}
